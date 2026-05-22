7af6d3343dbe8a8f41a2e92374fa7d891dcf066a
Ryan Ananias <141505101+RyanAnanias12@users.noreply.github.com>
2026-05-22 12:23:57 -0300

    minimizing server reads

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.main.routes.users_routes import users_router
 
-
+#initial server
 app = FastAPI()
 
 app.add_middleware(
